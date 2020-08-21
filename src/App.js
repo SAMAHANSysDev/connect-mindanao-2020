dc059530a79234f70a315e73a5ac5fd90c0fabf4
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-08-21 12:34:39 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://ci4.googleusercontent.com/proxy/Y32pXeJaawNK5_G-nvSHnP_J-q3y8pM0Pqizw2r0gnUb9OecvKGG13a80oHgiDMRZVMXrEu8vhVLsoGA90eVWhazLZlH5Yop5A=s0-d-e1-ft#https://samahan-cdn.imfast.io/samahan_logo_colored.png" className="App-logo" alt="logo" />
+        <p>
+          Hello TSO!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
